3ef78211a2f7c9dbfd92329e156d5b476f736244
Your Name <you@example.com>
2021-08-10 18:59:28 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/virtual_events/VirtualEventsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.virtual_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/virtual_events/VirtualEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.virtual_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
