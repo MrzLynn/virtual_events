5bc736ee0071c97cf4c6a05b7e164977a3beb0ca
Your Name <you@example.com>
2021-09-27 17:16:32 -0500

    Tweaked html a tad

--- src/main/java/org/launchcode/virtual_events/controllers/EventController.java
+++ src/main/java/org/launchcode/virtual_events/controllers/EventController.java
@@ -58,8 +58,11 @@ public class EventController {
         }
         model.addAttribute(new Event());
         model.addAttribute(eventRepository.save(newEvent));
+        model.addAttribute("events",events);
         return "events/create";
     }
+
+
 
 
 }
